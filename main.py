b29292676f44c4d6c1a92e10e6cc6f572e760442
Koushik Paul <86985403+KoushikP04@users.noreply.github.com>
2023-05-13 23:11:46 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from blockchain import Blockchain
+
+print("Welcome to the Ultimate Mini Blockchain Simulator!")
+
+local_blockchain = Blockchain()
+
+while True:
+
+    str = input("Would you like to append a new block? Y or N: ")
+
+    if str == "Y":
+        sender = input("Who is the sender?: ")
+        reciever = input("Who is the receiver?: ")
+        amount = input("How much money was transferred in the transaction?: ")
+
+        block_to_be_added = {"sender": sender, "reciever": reciever, "amount": amount}
+
+        local_blockchain.add_block(block_to_be_added)
+
+   
+
+    else:
+        print("Please enter Y or N")
+
